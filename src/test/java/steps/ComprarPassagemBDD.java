fabf1d1df5a41bfa00271e44196ed26a6fe56042
David Medeiros <davidmedeiros94@hotmail.com>
2025-03-31 18:01:12 -0300

    criado estrutura

--- src/test/java/steps/ComprarPassagemBDD.java
+++ src/test/java/steps/ComprarPassagemBDD.java
@@ -1,29 +1,28 @@
 package steps;
 
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.E;
 import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
 
 public class ComprarPassagemBDD {
     @Dado("que acesso o site {string}")
     public void que_acesso_o_site(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        
     }
 
     @Quando("seleciono a origem {string} e destino {string}")
     public void seleciono_a_origem_e_destino(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        
     }
 
-    @Quando("clico no botão Find Flights")
+    @E("clico no botão Find Flights")
     public void clico_no_botão_find_flights() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        
     }
 
     @Entao("visualiza a lista de voos")
     public void visualiza_a_lista_de_voos() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        
     }
 }
