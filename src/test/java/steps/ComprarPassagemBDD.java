2e2adc2841b1dd466dd03a53bdcb03eba82eea24
David Medeiros <davidmedeiros94@hotmail.com>
2025-03-31 21:37:29 -0300

    instalado dependências e estruturado Before e After

--- src/test/java/steps/ComprarPassagemBDD.java
+++ src/test/java/steps/ComprarPassagemBDD.java
@@ -1,11 +1,37 @@
 package steps;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.E;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
 
 public class ComprarPassagemBDD {
+
+    WebDriver driver; // Objeto do Selenium WB
+
+    @Before
+    public void iniciar(){
+        WebDriverManager.chromedriver().setup(); // Garantir o driver correto para o Chrome
+        driver = new ChromeDriver(); // Instaciar como ChromeDriver
+
+        // Definir um tempo de espera implícita de 3 segundos
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(3000)); 
+        driver.manage().window().maximize(); // Exibir janela maximizada
+    }
+
+    @After
+    public void finalizar(){
+        driver.quit(); // Finalizar objeto do Selenium WB
+    }
+
     @Dado("que acesso o site {string}")
     public void que_acesso_o_site(String string) {
         
